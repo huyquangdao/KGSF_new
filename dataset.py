48124ff89bc3cbcd64f98bdbbf0a90b25136b225
huyquangdao <=>
2021-10-05 17:20:59 +0700

    change word item graph

--- dataset.py
+++ dataset.py
@@ -185,7 +185,7 @@ class dataset(object):
         self.word2index = json.load(open("word2index_redial.json", encoding="utf-8"))
         self.key2index = json.load(open("key2index_3rd.json", encoding="utf-8"))
 
-        self.movie_keywords = json.load(open('new_attribute_1.json'))
+        self.movie_keywords = json.load(open('new_attribute_genres_company.json'))
         print(len(self.movie_keywords))
 
         self.non_item_entities = []
