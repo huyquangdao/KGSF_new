f25d4eb62885191ef7c9b78cc1e887fddbaba1cf
huyquangdao <=>
2021-11-03 10:19:55 +0700

    change word item graph

--- dataset.py
+++ dataset.py
@@ -134,9 +134,9 @@ class dataset(object):
         all_lens = []
         num_edges = 0
         for sample in self.movie_keywords:
-            key_words = sample['keywords'][:40]
+            key_words = sample['keywords']
             
-            re_tokenized_keywords = [word_tokenize(x) for x in key_words]
+            re_tokenized_keywords = [word_tokenize(x) for x in key_words][:30]
             re_tokenized_keywords = [word for words in re_tokenized_keywords for word in words if word in self.key2index]
 
             sample['keywords'] = list(set(re_tokenized_keywords))
